36f34fccac2bf2cb03bddf86d8361a36b5245776
LinaKind <lina.kinduryte@gmail.com>
2022-01-04 21:53:24 +0000

    toggle and remove methods

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="task">
-         <h3>
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
         {{task.text}}
+        <i @click="onDelete(task.id)" class="fas fa-times"></i>
     </h3>
     <p>{{task.day}}</p>
 
@@ -13,11 +14,19 @@
         name: "Task",
         props: {
             task: Object
-        }
+        },
+        methods: {
+            onDelete(id) {
+                this.$emit("delete-task", id)
+            }
+        },
     }
 </script>
 
 <style >
+.fas {
+    color: red;
+}
 .task {
     background: #f4f4f4;
     margin: 5px;

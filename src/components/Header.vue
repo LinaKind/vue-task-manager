2480a288e56673bd19f70df260b6e402e8bc8428
LinaKind <lina.kinduryte@gmail.com>
2022-01-04 21:00:48 +0000

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>
+            {{title}}
+        </h1>
+        <Button text="Add Task" color="green"/>
+    </header>
+</template>
+
+
+<script>
+    import Button from "./Button.vue"
+    export default {
+        name: "Header",
+        props: {
+            title: String
+        },
+        components: {
+            Button
+        }
+    }
+</script>
+
+<!-- style scoped means will only be used for this specific component-->
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+</style>
